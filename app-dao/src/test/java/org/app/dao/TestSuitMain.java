bd5fa2bb9f9a579b45bf599a3cb5c8bbcb3520e5
qnislhxxsx <15017941138@163.com>
2016-11-17 12:08:52 +0800

    Add files via upload

---
+++ app-dao/src/test/java/org/app/dao/TestSuitMain.java
@@ -0,0 +1,11 @@
+package org.app.dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SampleMapperTest.class })
+public class TestSuitMain {
+
+}
